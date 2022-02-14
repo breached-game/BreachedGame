174e99880cce13132e52c9a426835f41c3538caf
Luke Benson <ldunkinb@gmail.com>
2022-02-14 17:09:54 +0000

    Added drop off

--- Assets/Scripts/DropOff.cs
+++ Assets/Scripts/DropOff.cs
@@ -6,16 +6,18 @@ public class DropOff : MonoBehaviour
 {
     public string itemToBeDroppedOff;
 
-    public void droppingOffItem(string item, GameObject Player)
+    public void droppingOffItem(string itemCarried, GameObject Player)
     {
-        if(item == itemToBeDroppedOff)
+        if(itemCarried == itemToBeDroppedOff)
         {
             print(itemToBeDroppedOff + " has been dropped off");
+            //reset carry field
             Player.GetComponent<PlayerManager>().objectPlayerHas = null;
+            //respawn object 
         }
         else
         {
-            print("Player does not have " + item + "instead it has " + Player.GetComponent<PlayerManager>().objectPlayerHas);
+            print("Player does not have " + itemCarried + "instead it has " + Player.GetComponent<PlayerManager>().objectPlayerHas);
         }
     }
 }
