a783043a9b94106b11dbebfbcfda7361b1b2b1ee
sbarnesthornton <sepbt@btinternet.com>
2022-02-19 11:50:54 +0000

    Alarm audio from lights

--- Assets/Scripts/StartGameButton.cs
+++ Assets/Scripts/StartGameButton.cs
@@ -7,11 +7,13 @@ public class StartGameButton : MonoBehaviour
     public GameObject spawnPoint;
     public GameObject[] players;
     public GameObject playerUI;
+    public GameObject lights;
 
     public void startGame()
     {
         //Bad practice we should pass players in some other way 
         players = GameObject.FindGameObjectsWithTag("Player");
+        lights.GetComponent<AudioSource>().Play();
         foreach(GameObject player in players)
         {
             player.transform.position = spawnPoint.transform.position;
