127cafca1d67ca21da6b533030c139c13967f9e7
AndrewMorgan2 <58256684+AndrewMorgan2@users.noreply.github.com>
2022-02-20 16:26:54 +0000

    Revert "Update PlayerUIManager.cs"
    This reverts commit 6861b54d904a0b6f27d0c2e8d6cd8facc312c082.

--- Assets/Scripts/PlayerUI/PlayerUIManager.cs
+++ Assets/Scripts/PlayerUI/PlayerUIManager.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 using TMPro;
 using Mirror;
 
-public class SyncDictionaryStringString : SyncDictionary<string, string> { }
-
-public class PlayerUIManager : NetworkBehaviour
+public class PlayerUIManager : MonoBehaviour
 {
     public GameObject playerHoldingText;
     public GameObject prefabObjectiveName;
     public GameObject prefabObjectiveDescription;
-
-    public readonly SyncDictionaryStringString currentObjectives = new SyncDictionaryStringString();
-    public readonly SyncDictionaryStringString doneObjectives = new SyncDictionaryStringString();
 
     public Color doneObjectTextColour;
     public Color objectTextColour;
@@ -21,18 +16,11 @@ public class PlayerUIManager : NetworkBehaviour
     private List<GameObject> UIElements =  new List<GameObject>();
     private float offsetY = 5;
 
-    public override void OnStartClient()
-    {
-        currentObjectives.Callback += OnObjectiveChange;
-        doneObjectives.Callback += OnObjectiveChange;
-    }
-
     public void UpdatePlayerHolding(string itemName)
     {
         playerHoldingText.GetComponent<TextMeshProUGUI>().text = itemName;
     }
-
-    public void UpdateObjectiveUI()
+    public void updateObjectiveList(Dictionary<string, string> objectives, Dictionary<string, string> doneObjectives)
     {
         //Clear UI
         offsetY = 5;
@@ -52,46 +40,11 @@ public class PlayerUIManager : NetworkBehaviour
             }
         }
 
-        foreach (var objective in currentObjectives)
+        foreach (var objective in objectives)
         {
             string objectiveName = objective.Key;
             string objectiveDescription = objective.Value;
             createObjectiveBox(objectiveName, objectiveDescription, objectTextColour);
-        }
-    }
-    public void updateObjectiveList(Dictionary<string, string> objectives, Dictionary<string, string> completedObjectives)
-    {
-        currentObjectives.Clear();
-        doneObjectives.Clear();
-        foreach (var objective in objectives)
-        {
-            currentObjectives.Add(objective);
-        }
-        foreach (var objective in completedObjectives)
-        {
-            doneObjectives.Add(objective);
-        }
-        UpdateObjectiveUI();
-    }
-
-    public void OnObjectiveChange(SyncDictionaryStringString.Operation op, string key, string value)
-    {
-        switch (op)
-        {
-            case SyncIDictionary<string, string>.Operation.OP_ADD:
-                if (currentObjectives.Count + doneObjectives.Count == 3)
-                {
-                    UpdateObjectiveUI();
-                }
-                break;
-            case SyncIDictionary<string, string>.Operation.OP_CLEAR:
-                break;
-            case SyncIDictionary<string, string>.Operation.OP_REMOVE:
-                break;
-            case SyncIDictionary<string, string>.Operation.OP_SET:
-                break;
-            default:
-                break;
         }
     }
 
