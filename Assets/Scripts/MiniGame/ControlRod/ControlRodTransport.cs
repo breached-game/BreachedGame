2f419a0ce25feab3eb73e2929a52bc6b618b8e15
AndrewMorgan2 <58256684+AndrewMorgan2@users.noreply.github.com>
2022-03-13 20:53:02 +0000

    Added Simple animation for in minigame

--- Assets/Scripts/MiniGame/ControlRod/ControlRodTransport.cs
+++ Assets/Scripts/MiniGame/ControlRod/ControlRodTransport.cs
@@ -10,6 +10,9 @@ public class ControlRodTransport : NetworkBehaviour
     public GameObject controlRod;
     public GameObject playerUI;
 
+    public GameObject playerPos;
+    private Vector3 prePlayerPos;
+
     [SyncVar]
     public GameObject currentPlayer = null;
 
@@ -24,6 +27,7 @@ public class ControlRodTransport : NetworkBehaviour
                 CmdSendCurrentPlayer(player);
                 Cursor.lockState = CursorLockMode.None;
                 player.gameObject.GetComponent<PlayerManager>().FirstPersonCamera.SetActive(false);
+                VisualEffectOfBeingInTheMiniGame(player);
                 //STOPPING PLAYER MOVING WHILE IN CONTROL ROD - MAYBE CHANGE PLAYERMANAGER TO HAVE A BOOL INSTEAD OF SETTING IT AS 0
                 player.gameObject.GetComponent<PlayerManager>().Speed = 0.0f;
                 controlRodCamera.SetActive(true);
@@ -45,6 +49,7 @@ public class ControlRodTransport : NetworkBehaviour
             if (currentPlayer.GetComponent<NetworkIdentity>().isLocalPlayer)
             {
                 currentPlayer.GetComponent<PlayerManager>().FirstPersonCamera.SetActive(true);
+                VisualEffectOfLeavingAMiniGame(currentPlayer);
                 //TRASH CODING PRACTICE INBUILT SPEED
                 currentPlayer.GetComponent<PlayerManager>().Speed = 4.0f;
                 currentPlayer.GetComponent<PlayerManager>().disableInteractionsForMinigame = false;
@@ -70,12 +75,6 @@ public class ControlRodTransport : NetworkBehaviour
     {
         currentPlayer = player;
     }
-    /*[ClientRpc]
-    public void CmdUpdateCurrentPlayer(GameObject player)
-    {
-        currentPlayer = player;
-    }*/
-
 
     [Command]
     public void CmdMoveControlRod(Vector3 direction)
@@ -88,5 +87,20 @@ public class ControlRodTransport : NetworkBehaviour
     public void CmdUpdateControlRodMovement(Vector3 force)
     {
         controlRod.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+    }
+    //Visual effects
+    public void VisualEffectOfBeingInTheMiniGame(GameObject player)
+    {
+        GameObject playerModel = player.GetComponent<PlayerManager>().PlayerModel;
+        playerModel.GetComponent<Animator>().Play("SitDown");
+        prePlayerPos = playerModel.transform.position;
+        playerModel.transform.position = playerPos.transform.position;
+        player.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+    public void VisualEffectOfLeavingAMiniGame(GameObject player)
+    {
+        GameObject playerModel = player.GetComponent<PlayerManager>().PlayerModel;
+        playerModel.GetComponent<Animator>().Play("Idle");
+        playerModel.transform.position = prePlayerPos;
     }
 }
