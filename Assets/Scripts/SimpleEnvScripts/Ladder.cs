15f270d1fe0a35e65141279c0a6ea9aca944a422
sbarnesthornton <sepbt@btinternet.com>
2022-03-28 15:25:15 +0100

    Added Climbing

--- Assets/Scripts/SimpleEnvScripts/Ladder.cs
+++ Assets/Scripts/SimpleEnvScripts/Ladder.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class Ladder : MonoBehaviour
 {
-    public GameObject climbPos;
+    public GameObject bottomPos;
+    public GameObject topPos;
+    private Vector3 originalPos;
 
     public void ClimbLadder(GameObject player)
     {
-        player.transform.position = climbPos.transform.position;
+        print("Started climbing");
         Animator playerAni = player.GetComponent<Animator>();
         playerAni.Play("StartClimbing");
+        originalPos = player.transform.position;
+        if ((player.transform.position - topPos.transform.position).magnitude < (player.transform.position - bottomPos.transform.position).magnitude)
+        {
+            player.transform.position = topPos.transform.position;
+            StartCoroutine(PlayerClimb(topPos.transform.position, bottomPos.transform.position, player));
+        }
+        else { 
+            player.transform.position = bottomPos.transform.position;
+            StartCoroutine(PlayerClimb(bottomPos.transform.position, topPos.transform.position, player));
+        }
         playerAni.SetBool("Ladder", true);      
+    }
+
+    IEnumerator PlayerClimb(Vector3 startPos, Vector3 stopPos, GameObject player)
+    {
+        float t = 0.01f;
+        while (t <= 1)
+        {
+            player.transform.position = Vector3.Lerp(startPos, stopPos, t);
+            yield return new WaitForSeconds(0.01f);
+        }
+        originalPos.y = stopPos.y;
+        player.transform.position = originalPos;
+        player.GetComponent<Animator>().SetBool("Ladder", false);
     }
 }
