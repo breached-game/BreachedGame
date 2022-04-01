801e48e2b00b508cafb9ccc9cdfa2d1520bc67e8
sbarnesthornton <sepbt@btinternet.com>
2022-04-01 11:32:08 +0100

    Added don't destroy on load

--- Assets/Scripts/Voice Chat/MicManagerScript.cs
+++ Assets/Scripts/Voice Chat/MicManagerScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MicManagerScript : MonoBehaviour
 {
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
