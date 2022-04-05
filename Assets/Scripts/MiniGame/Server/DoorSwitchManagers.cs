3865df2215aaa5114db221546ae1403de0e919a7
seth9022 <72393714+seth9022@users.noreply.github.com>
2022-04-05 18:09:14 +0100

    FIRST TIME ! Got server doors to open and shut slowly

--- Assets/Scripts/MiniGame/Server/DoorSwitchManagers.cs
+++ Assets/Scripts/MiniGame/Server/DoorSwitchManagers.cs
@@ -45,45 +45,22 @@ public class DoorSwitchManagers : MonoBehaviour
     {
         for(int i = 0; i<currentDoorState.Count; i++)
         {
-            bool active;
+      
             if(currentDoorState[i] == 0)
             {
-                active = true;
+
+                StartCoroutine(doors[i].GetComponent<ServerDoorScript>().CloseDoor());
+                
             }
             else
             {
-                active = false;
+                StartCoroutine(doors[i].GetComponent<ServerDoorScript>().OpenDoor());
             }
 
-            
-            doors[i].GetComponent<MeshRenderer>().enabled = active;
-            doors[i].GetComponent<BoxCollider>().enabled = active;
+ 
 
 
         }
-
-        /*print(currentDoorState.Contains(1));
-        for (int i = 0; i < currentDoorState.Count; i++)
-        {
-            Animator aniDoor = doors[i].GetComponent<Animator>();
-            if (currentDoorState[i] == 0) 
-            {
-                if (aniDoor.GetCurrentAnimatorStateInfo(0).IsName("Closed"))
-                {
-                    aniDoor.GetComponent<Animator>().Play("Closed");
-                    // aniDoor.GetComponent<Animator>().Play("Open");
-                }
-            }
-            else 
-            {
-                if (aniDoor.GetCurrentAnimatorStateInfo(0).IsName("Open"))
-                {
-                   // aniDoor.GetComponent<Animator>().Play("Closed");
-                    aniDoor.GetComponent<Animator>().Play("Open");
-                }
-            }
-
-        }*/
 
     }
 
