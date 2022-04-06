47b8b4f527a5ad485bf62b5133af93cad5e78db7
sbarnesthornton <sepbt@btinternet.com>
2022-04-06 12:03:56 +0100

    Changed Command Text

--- Assets/LobbySetup.cs
+++ Assets/LobbySetup.cs
@@ -11,7 +11,7 @@ public class LobbySetup : MonoBehaviour
     {
         commandManager = commandObject.GetComponent<CommandManager>();
         commandManager.QueueMessage("Welcome to the lobby area!");
-        commandManager.QueueMessage("Please wait until all of your team are in the lobby to press the red button");
+        commandManager.QueueMessage("Press that red button when your whole team is in the lobby");
         commandManager.QueueMessage("Make sure you have adjusted your brightness using the slider in your settings menu (esc)");
     }
 }
