e4cf3110df3a9126bf38718458682fa63d6978c5
sbarnesthornton <sepbt@btinternet.com>
2022-04-07 22:00:03 +0100

    Revert "Server Update"
    This reverts commit daa515f4cc6d55445c6455846861256630e37674.

--- Assets/Scripts/Networking/MyNetworkManager.cs
+++ Assets/Scripts/Networking/MyNetworkManager.cs
@@ -7,21 +7,25 @@ public class MyNetworkManager : NetworkManager
 {
     public override void OnStartServer()
     {
+        base.OnStartServer();
         Debug.Log("Server Started!");
     }
 
     public override void OnStopServer()
     {
+        base.OnStopServer();
         Debug.Log("Server Stopped!");
     }
 
     public override void OnClientConnect(NetworkConnection conn)
     {
+        base.OnClientConnect(conn);
         Debug.Log("Connected to Server!");
     }
 
     public override void OnServerDisconnect(NetworkConnection conn)
     {
+        base.OnServerDisconnect(conn);
         Debug.Log("Disconnected from Server!");
         playerExitsOrDisconnects(conn);
     }
