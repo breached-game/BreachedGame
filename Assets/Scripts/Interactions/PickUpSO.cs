f9df26be8a1dfa95b7371cce899e46ca4ccda36b
sbarnesthornton <sepbt@btinternet.com>
2022-04-07 22:01:39 +0100

    Revert "Server change to fix bug"
    This reverts commit 7ffa3f80d254ba95d2d199f759b5b0e65b973f81.

--- Assets/Scripts/Interactions/PickUpSO.cs
+++ Assets/Scripts/Interactions/PickUpSO.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 [CreateAssetMenu(fileName = "InteractionScriptableObject", menuName = "Interactions/PickUp")]
 public class PickUpSO : InteractionSO
 {
