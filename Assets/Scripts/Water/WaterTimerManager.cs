d1ef1a7c47676424b6d99715ae0cb88e662dfe6a
srdjanVojnovic <ig19310@bristol.ac.uk>
2022-04-10 22:27:28 +0100

    Multiple water grids after time working

--- Assets/Scripts/Water/WaterTimerManager.cs
+++ Assets/Scripts/Water/WaterTimerManager.cs
@@ -6,17 +6,21 @@ public class WaterTimerManager : MonoBehaviour
 {
     public float waterGridTimer = 30f;
     public GameObject waterGrid;
+    private bool run = true;
 
     void Update()
     {
-        if (waterGridTimer > 0)
-        {
-            waterGridTimer -= Time.deltaTime;
-        }
-        if (waterGridTimer < 0)
-        {
-            gameObject.GetComponent<DropOffMiniGameManager>().Reset();
-            waterGrid.SetActive(true);
+        if (run) {
+            if (waterGridTimer > 0)
+            {
+                waterGridTimer -= Time.deltaTime;
+            }
+            if (waterGridTimer < 0)
+            {
+                gameObject.GetComponent<DropOffMiniGameManager>().Reset();
+                waterGrid.SetActive(true);
+                run = false;
+            }
         }
     }
 }
