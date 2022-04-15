f089c5c3de4f0375ea3e2a024153483ddef7e81b
srdjanVojnovic <ig19310@bristol.ac.uk>
2022-04-15 01:19:08 +0100

    MinigameManager script testing done

--- Assets/Scripts/GameScripts/UIManager/UISpritesAnimation.cs
+++ Assets/Scripts/GameScripts/UIManager/UISpritesAnimation.cs
@@ -22,13 +22,16 @@ public class UISpritesAnimation : MonoBehaviour
     }
     IEnumerator playGif()
     {
-        for (int i = 0; i < sprites.Length; i++)
+        if (sprites != null)
         {
-            //i need help here i have no idea why image is null sometimes -Andy
-            if (image != null) image.sprite = sprites[i];
-            yield return new WaitForSeconds(duration);
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                //i need help here i have no idea why image is null sometimes -Andy
+                if (image != null) image.sprite = sprites[i];
+                yield return new WaitForSeconds(duration);
+            }
+            gameObject.SetActive(false);
         }
-        gameObject.SetActive(false);
     } 
 }
 
