77e4fe9328a622d428020606069cbed4b4287b13
seth9022 <72393714+seth9022@users.noreply.github.com>
2022-04-27 14:47:27 +0100

    Moved camera

--- Assets/Scripts/GameScripts/Minimap/MinigameCameraScript.cs
+++ Assets/Scripts/GameScripts/Minimap/MinigameCameraScript.cs
@@ -7,6 +7,7 @@ public class MinigameCameraScript : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+
         DontDestroyOnLoad(this.gameObject);
 
     }
